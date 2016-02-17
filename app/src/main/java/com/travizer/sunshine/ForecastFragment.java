830b5b6197fcdd6a44c22f66a14ea9329c00ffb1
mdimkov1 <mdimkov@hotmail.com>
2016-02-17 19:45:25 +0200

    after creating dynamic URL and implementing a Refresh menu option

--- app/src/main/java/com/travizer/sunshine/ForecastFragment.java
+++ app/src/main/java/com/travizer/sunshine/ForecastFragment.java
@@ -1,9 +1,14 @@
 package com.travizer.sunshine;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -19,7 +24,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-//import android.widget.Toast;
+import android.widget.Toast;
 import android.util.Log;
 
 /**
@@ -28,6 +33,12 @@ import android.util.Log;
 public class ForecastFragment extends Fragment {
 
     public ForecastFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -64,11 +75,33 @@ public class ForecastFragment extends Fragment {
         return v;
     }
 
-    public class FetchWeatherTask extends AsyncTask<Void, Void, Void>{
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_refresh){
+            FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
+            fetchWeatherTask.execute("94043,US");
+
+            int permissionCheck = ContextCompat.checkSelfPermission(getActivity(),
+                    "android.permission.INTERNET");
+
+            Toast.makeText(getActivity(),"permission check: " + Integer.toString(permissionCheck),Toast.LENGTH_LONG).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, Void>{
         private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
 
         @Override
-        protected Void doInBackground(Void... params) {
+        protected Void doInBackground(String... params) {
 
             // These two need to be declared outside the try/catch
             // so that they can be closed in the finally block.
@@ -82,7 +115,34 @@ public class ForecastFragment extends Fragment {
                 // Construct the URL for the OpenWeatherMap query
                 // Possible parameters are available at OWM's forecast API page, at
                 // http://openweathermap.org/API#forecast
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+
+                //declare the URL base
+                final String THE_URL_BASE = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+
+                //declare the query params
+                final String QUERY_PARAM = "q";
+                final String FORMAT_PARAM = "mode";
+                final String UNITS_PARAM = "units";
+                final String DAYS_PARAM = "cnt";
+                final String APP_ID = "appid";
+
+                //declare the query param values
+                String format = "json";
+                String untis = "metric";
+                String appId = "44db6a862fba0b067b1930da0d769e98";
+                int numDays = 7;
+
+                Uri builtURI = Uri.parse(THE_URL_BASE).buildUpon()
+                        .appendQueryParameter(QUERY_PARAM, params[0])
+                        .appendQueryParameter(FORMAT_PARAM, format)
+                        .appendQueryParameter(UNITS_PARAM, untis)
+                        .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
+                        .appendQueryParameter(APP_ID, appId)
+                        .build();
+
+                URL url = new URL(builtURI.toString());
+
+                Log.v("MDIMKOV", url.toString());
 
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
